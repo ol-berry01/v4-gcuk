07cb3e4d53d449ca439a9dc1dc358f04f1b66ae2
Oliver Berry <subscriptions@littlemonsterdigital.co.uk>
2018-04-03 13:41:43 +0100

    Update - v4.2.2.2

--- template-contact.php
+++ template-contact.php
@@ -19,47 +19,29 @@ get_header(); ?>
 		<div class="container">
             <div class="row wow fadeInUp">
             <!-- Breadcrumbs -->
-                <div class="breadcrumbs">
+                <div class="col-xs-12 breadcrumbs">
                     <?php if ( function_exists('yoast_breadcrumb') ) {
                         yoast_breadcrumb('<div id="breadcrumbs">','</div>');
                     } ?>
                 </div>
+                
             <!-- End: Breadcrumbs -->
-                <div class="title-section">
-                    <div class="section-title">
-                        <h1>Contact Us</h1>
-                        <p class="lead">We are here to answer any questions you may have about any of our products or services</p>
-                    </div>
+                <div class="col-xs-12">
+                    <h1>Contact Us</h1>
+                    <p class="lead">We are here to answer any questions you may have about any of our products or services</p>
                 </div>
-                <div class="contact page">
-                    <div class="col-sm-8">
-                        <div class="col-left" style="background:#fff;padding:50px;border-radius:3px;box-shadow:3px 3px 5px rgba(51,51,51,0.3)">
+                
+                    <div class="col-md-8">
+                        <div style="background:#fff;padding:50px;border-radius:3px;box-shadow:3px 3px 5px rgba(51,51,51,0.3)">
                             <h3 style="margin-bottom:15px;">Send Us a Message</h3>
                             <p class="lead">We'll always respond as soon as we can</p>
 
-				<?php echo do_shortcode( '[contact-form-7 id="1753" title="Contact Page - Form"]' ); ?>
-
-                            <!-- <form id="form" class="topBefore">
-                                <input id="name" type="text" placeholder="Name*">
-                                <input id="email" type="text" placeholder="E-Mail*">
-                                <input id="company" type="text" placeholder="Company">
-                                <select id="list" type="select" name="departments">
-                                    <option value="general">General Enquiry</option>
-                                    <option value="sales">Vehicle Sales</option>
-                                    <option value="rental">Vehicle Rental</option>
-                                    <option value="maintanence">Maintanence</option>
-                                    <option value="parts">Parts</option>
-                                    <option value="support">Support</option>
-                                </select>
-                                <textarea id="message" type="text" placeholder="Message"></textarea>
-                                <input id="captcha" type="text" placeholder="Captcha*, what is 5+9?">
-                                <input id="submit" type="submit" value="Send">
-                                <p style="margin-top:10px">*Denotes requried fields, so we can get back to you and check you're real</p>
-                            </form> -->
+				            <?php echo do_shortcode( '[contact-form-7 id="1753" title="Contact Page - Form"]' ); ?>
+                            
                         </div>
                     <!-- Contact Page Right -->
                     </div>
-                    <div class="col-sm-4">
+                    <div class="col-md-4">
                         <div class="col-right" style="padding:50px">
                             
                             <h3 style="margin-bottom:15px;">General Information</h3>
@@ -77,7 +59,6 @@ get_header(); ?>
                         </div>
                     </div>
                     <!-- End: Contact Page Right -->
-                </div>
             </div>
             <!-- End: Row -->
         </div>
